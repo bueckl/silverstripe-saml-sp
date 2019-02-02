af0fd4fcffd12ea39a0b4ab391c37605f6dc088e
jochenguelden <jochen@guelden.org>
2019-02-02 20:12:25 +0100

    move config to poroject config so we can bybass saml

--- _config.php
+++ _config.php
@@ -1,11 +1,14 @@
 <?php
 
 // @TODO re-routing should only work when it is enabled in config and there is an active IdP.
-if (Config::inst()->get('SingleSignOnConfig', 'EnableSingleSignOn')) {
-	// process all request of '/security' with the SAML controller.
-	Config::inst()->update('Director', 'rules', array(
-		'Security//$Action/$ID/$OtherID' => 'SAMLSecurity',
-		'security//$Action/$ID/$OtherID' => 'SAMLSecurity'
-	));
-}
 
+// Gets set in config, so we can bypass saml authetication and locally log in to the cms
+
+// if (Config::inst()->get('SingleSignOnConfig', 'EnableSingleSignOn')) {
+//     // process all request of '/security' with the SAML controller.
+//     Config::inst()->update('Director', 'rules', array(
+//         'Security//$Action/$ID/$OtherID' => 'SAMLSecurity',
+//         'security//$Action/$ID/$OtherID' => 'SAMLSecurity'
+//     ));
+// }
+//
